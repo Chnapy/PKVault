7b1517caf3c6e06ad18dc3f41b76161773393794
Richard Haddad <richardhaddad@hotmail.fr>
2026-01-09 02:02:53 +0100

    fix: broken debug console

--- PKVault.WinForm/Program.cs
+++ PKVault.WinForm/Program.cs
@@ -1,30 +1,34 @@
+using System.Runtime.InteropServices;
+
 namespace PKVault.WinForm;
 
 public static class Program
 {
+#if DEBUG
+        [DllImport("kernel32.dll")]
+        static extern bool AttachConsole(int dwProcessId);
+#endif
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
+#if DEBUG
+                AttachConsole(-1);
+                Console.WriteLine("Debug Console");
+#endif
+
                 LogUtil.Initialize();
 
                 // To customize application configuration such as set high DPI settings or default font,
                 // see https://aka.ms/applicationconfiguration.
                 ApplicationConfiguration.Initialize();
 
-#if DEBUG
-                NativeMethods.AllocConsole();
-                Console.WriteLine("Debug Console");
-#endif
 
                 Backend.Program.Copyright();
                 Application.Run(new MainForm(args));
-
-#if DEBUG
-                NativeMethods.FreeConsole();
-#endif
 
                 LogUtil.Dispose();
         }
