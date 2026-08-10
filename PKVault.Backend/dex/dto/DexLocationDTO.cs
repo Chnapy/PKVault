716e5fa2c300dd029f01c684cdd6905dfbfdff4a
Richard Haddad <richardhaddad@hotmail.fr>
2026-08-11 01:19:18 +0200

    fix: broken compare in trimmed mode (#173)

--- PKVault.Backend/dex/dto/DexLocationDTO.cs
+++ PKVault.Backend/dex/dto/DexLocationDTO.cs
@@ -19,8 +19,42 @@ public record DexLocationItem(
     // string? EggLocation,
     HashSet<DexLevelRange> Levels,
     AbilityPermission AbilitiesAllowed
-    // int? FixedBall,
-    // Shiny ShinyProbability
-);
+// int? FixedBall,
+// Shiny ShinyProbability
+) : IEquatable<DexLocationItem>
+{
+    public virtual bool Equals(DexLocationItem? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+
+        return Forms.SetEquals(other.Forms)
+            && EncounterType == other.EncounterType
+            && EncounterWithMethod == other.EncounterWithMethod
+            && IsEgg == other.IsEgg
+            && IsShiny == other.IsShiny
+            && Location == other.Location
+            && Levels.SetEquals(other.Levels)
+            && AbilitiesAllowed == other.AbilitiesAllowed;
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+
+        foreach (var f in Forms.Order())
+            hash.Add(f);
+        hash.Add(EncounterType);
+        hash.Add(EncounterWithMethod);
+        hash.Add(IsEgg);
+        hash.Add(IsShiny);
+        hash.Add(Location);
+        foreach (var l in Levels.Order())
+            hash.Add(l);
+        hash.Add(AbilitiesAllowed);
+
+        return hash.ToHashCode();
+    }
+};
 
 public record DexLevelRange(byte LevelMin, byte LevelMax);
