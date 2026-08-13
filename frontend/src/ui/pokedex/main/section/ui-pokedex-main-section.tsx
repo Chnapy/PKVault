2fe3d5e71ea6784a67c703a83c32a51eb0d3a2ac
Richard Haddad <richardhaddad@hotmail.fr>
2026-08-13 15:33:36 +0200

    fix: pokedex items rendering performances

--- frontend/src/ui/pokedex/main/section/ui-pokedex-main-section.tsx
+++ frontend/src/ui/pokedex/main/section/ui-pokedex-main-section.tsx
@@ -1,12 +1,17 @@
 import { Group } from '@mantine/core';
 import React from 'react';
+import { VisibilityObserver } from '../../../visibility/visibility-observer';
 
 type UIPokedexMainSectionProps = {
     children: React.ReactNode;
 };
 
 export const UIPokedexMainSection: React.FC<UIPokedexMainSectionProps> = ({ children }) => {
-    return <Group gap='sm' py='md'>
-        {children}
+    const ref = React.useRef<HTMLDivElement>(null);
+
+    return <Group ref={ref} gap='sm' py='md'>
+        <VisibilityObserver ref={ref} margin='600px'>
+            {children}
+        </VisibilityObserver>
     </Group>;
 };
