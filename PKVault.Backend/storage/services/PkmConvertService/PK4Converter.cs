85a3404b0260f1e28e5583a3a320958fa401713a
Richard Haddad <richardhaddad@hotmail.fr>
2026-08-27 01:52:17 +0200

    fix: nickname case and PID changes after moving a PKM

--- PKVault.Backend/storage/services/PkmConvertService/PK4Converter.cs
+++ PKVault.Backend/storage/services/PkmConvertService/PK4Converter.cs
@@ -6,6 +6,8 @@ public class PK4Converter(PKMConverterUtils utils)
     public PK5 ConvertToPK5Fixed(PK4 pk4, PKMRndValues? rndValues)
     {
         var pk5 = pk4.ConvertToPK5();
+        if (rndValues != null)
+            pk5.PID = rndValues.PID;
 
         utils.FixMetLocation(pk5, [
             GameVersion.S, GameVersion.R, GameVersion.E, GameVersion.FR, GameVersion.LG, GameVersion.CXD,
