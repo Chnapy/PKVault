be4ce6fc61b4b4f2774dbde4f5ec3894dde1425b
Richard Haddad <richardhaddad@hotmail.fr>
2026-09-02 15:22:45 +0200

    feat: add mobile app project

---
+++ PKVault.Mobile/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace PKVault.Mobile;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+
+		// hybridWebView.
+	}
+
+	private void OnHybridWebViewInitializing(object? sender, EventArgs e) { Console.WriteLine("OnHybridWebViewInitializing"); }
+
+	private void OnHybridWebViewRawMessageReceived(object? sender, EventArgs e) { Console.WriteLine("OnHybridWebViewRawMessageReceived"); }
+
+	private void OnHybridWebViewWebResourceRequested(object? sender, EventArgs e) { Console.WriteLine("OnHybridWebViewWebResourceRequested"); }
+}
